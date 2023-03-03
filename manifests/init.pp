112ec21e5ad92d529fc64ea67c5f444228534ae4
kumar <kumar@puppet.com>
2023-03-03 17:59:50 +0800

    adding a new file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,15 @@
-class mkcustomfact::transfer_file {
-  file { 'example.txt':
-    ensure => present,
-    source => 'puppet:///modules/mkcustomfact/example.txt',
-    path   => 'C:\example.txt',
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include mkcustomfact::transfer_file
+class mkcustomfact {
+  if $::osfamily == 'windows' {
+    file { 'example.txt':
+      ensure  => present,
+      source => 'puppet:///modules/mkcustomfact/example.txt',
+      path   => 'C:\example.txt',
+    }
   }
 }
