6b6d51497d44b3adbbd00c1b15849a611d330983
kumar <kumar@puppet.com>
2023-04-04 13:11:05 +0800

    adding a new class for findfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,4 +12,16 @@ class mkcustomfact {
       path   => 'C:\example.txt',
     }
   }
+
+  $testhost = find_file('/etc/hosts')
+  notify {"Host file is in [${testhost}]":}
+  $loclscript = find_file('/usr/local/bin/elastic_agent_install.sh')
+  notify {"Local script exists in [${loclscript}] with side helping of [${testhost}]":}
+
+  file { '/tmp/check/elastic_agent_install.sh':
+    ensure => 'present',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
 }
